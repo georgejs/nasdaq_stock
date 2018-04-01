d6753f10f3d800b3656cfcba2521234d8ff1b801
GS <gjshen@gmail.com>
2018-04-01 12:36:55 -0500

    Added setup files for pypi

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'nasdaq_stock',
+  packages = ['nasdaq_stock'],
+  version = '0.0.1',
+  description = 'nasdaq stock retriever',
+  author = 'George Shen',
+  author_email = 'georgejs.arch@gmail.com',
+  url = 'https://github.com/georgejs/nasdaq_stock',
+  download_url = 'https://github.com/georgejs/nasdaq_stock/archive/0.0.1.tar.gz',
+  keywords = ['stock', 'nasdaq', 'stock retriever', 'nasdaq stock'],
+  classifiers = [],
+)
