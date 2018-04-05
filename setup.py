5dab64430788a4dcca9e44611d31609a20128d5d
GS <gjshen@gmail.com>
2018-04-04 20:02:20 -0500

    Try 2 updated version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
   author = 'George Shen',
   author_email = 'georgejs.arch@gmail.com',
   url = 'https://github.com/georgejs/nasdaq_stock',
-  download_url = 'https://github.com/georgejs/nasdaq_stock/archive/0.0.1.tar.gz',
+  download_url = 'https://github.com/georgejs/nasdaq_stock/archive/0.0.2.tar.gz',
   keywords = ['stock', 'nasdaq', 'stock retriever', 'nasdaq stock'],
   classifiers = [],
 )
