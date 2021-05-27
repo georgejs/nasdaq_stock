2f4418a7c009d5ae0d5a7dabef2f70dfa00a8f58
georgejs <gjshen@gmail.com>
2021-05-26 18:46:39 -0600

    Updated xpath for price

--- nasdaq_stock/nasdaq_stock.py
+++ nasdaq_stock/nasdaq_stock.py
@@ -11,7 +11,7 @@ from lxml import etree
 import datetime
 
 
-price_xp = '/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[4]/div/div/div/div[3]/div[1]/div/span[1]/text()'
+price_xp = '/html/body/div[1]/div/div/div[1]/div/div[2]/div/div/div[5]/div/div/div/div[3]/div[1]/div/span[1]/text()'
 range_xp = '/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[5]/td[2]/text()'
 vol_xp = '/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[7]/td[2]/span/text()'
 vol_avg_xp = '/html/body/div[1]/div/div/div[1]/div/div[3]/div[1]/div/div[1]/div/div/div/div[2]/div[1]/table/tbody/tr[8]/td[2]/span/text()'
