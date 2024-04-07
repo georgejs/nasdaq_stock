07292025f926a64dbdaaf23380fb49cc9ffb2830
georgejs <georgejs.arch@gmail.com>
2024-04-06 19:15:55 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ This will return string data and a dictionary of that data.\n \
 Current data fields are:                   \n \
 ticker: The stock or id of that ticker.    \n \
 date: The date when the stock was queried. \n \
-time: The time when the stock was taken. This can be helpful if you are trying to do minute by minute data gathering\n \
+time: The time when the stock was taken. This can be helpful if you are trying to do minute-by-minute data gathering\n \
 price: The current price or bid price      \n \
 ask: The current ask price                 \n \
 high: The high of the day                  \n \
@@ -28,13 +28,13 @@ market_cap: Market cap of the stock. i.e. dollar amount currently vested in stoc
 setup(
   name = 'nasdaq_stock',
   packages = ['nasdaq_stock'],
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'nasdaq stock retriever',
   long_description = long_description_text,
   author = 'George Shen',
   author_email = 'georgejs.arch@gmail.com',
   url = 'https://github.com/georgejs/nasdaq_stock',
-  download_url = 'https://github.com/georgejs/nasdaq_stock/archive/0.0.3.tar.gz',
+  download_url = 'https://github.com/georgejs/nasdaq_stock/archive/0.0.5.tar.gz',
   keywords = ['stock', 'nasdaq', 'stock retriever', 'nasdaq stock'],
   classifiers = [],
 )
